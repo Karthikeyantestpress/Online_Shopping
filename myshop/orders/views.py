1490b03124bd2c3dd93c1374d7765acaa196579e
Karthik <karthikeyan@testpress.in>
2022-11-21 17:32:23 +0530

    Add support to send an email when the order was placed

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import send_order_creation_mail
 
 
 def show_empty_orderform_to_place_order(request):
     cart = Cart(request)
-    form = OrderCreateForm()
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
+            cart.clear()
+            send_order_creation_mail.delay(order.id)
+            return render(
+                request, "orders/order/created.html", {"order": order}
+            )
+    else:
+        form = OrderCreateForm()
     return render(
         request, "orders/order/create.html", {"cart": cart, "form": form}
     )
