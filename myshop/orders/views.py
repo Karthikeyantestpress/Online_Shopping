8a50faff40a84b4a784e9ff0c22a7b3ba3116154
Karthik <karthikeyan@testpress.in>
2022-11-22 11:58:36 +0530

    Customize the admin look for the order model

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -4,6 +4,9 @@ from .models import OrderItem
 from .forms import OrderCreateForm
 from .tasks import send_order_creation_mail
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
 
 
 def show_empty_orderform_to_place_order(request):
@@ -52,3 +55,7 @@ def order_create(request):
     elif request.method == "POST":
         Response = show_order_confirmation_after_placing_the_order(request)
     return Response
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order/detail.html", {"order": order})
