e260b3de97b0085e45ab6bf07ac13d59d0dfbe60
Karthik <karthikeyan@testpress.in>
2022-11-22 13:08:37 +0530

    Add support to generate the invoice for an order in the PDF format

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -7,6 +7,10 @@ from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
 from .models import Order
+import weasyprint
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
 def show_empty_orderform_to_place_order(request):
@@ -59,3 +63,16 @@ def order_create(request):
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, "admin/orders/order/detail.html", {"order": order})
+
+
+@staff_member_required
+def generate_invoice_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string("orders/order/pdf.html", {"order": order})
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f"filename=order_{order.id}.pdf"
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + "css/pdf.css")],
+    )
+    return response
