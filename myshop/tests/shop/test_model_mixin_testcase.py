0f6e14c0356f1ce00687977a0a17777387f5b21b
Karthik <karthikeyan@testpress.in>
2022-11-25 12:21:05 +0530

    Refactor : Add testcases for productlist

--- myshop/tests/shop/test_model_mixin_testcase.py
+++ myshop/tests/shop/test_model_mixin_testcase.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from shop.models import Product, Category
+
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class ModelMixinTestCase(TestCase):
@@ -9,6 +11,10 @@ class ModelMixinTestCase(TestCase):
             name="Gaming",
             slug="gaming",
         )
+        self.newcategory2 = Category.objects.create(
+            name="Books",
+            slug="books",
+        )
 
         self.newproduct = Product.objects.create(
             category=self.newcategory,
@@ -16,4 +22,11 @@ class ModelMixinTestCase(TestCase):
             slug="play-station-5",
             description="A gaming console",
             price=50.000,
+        )
+        self.newproduct_2 = Product.objects.create(
+            category=self.newcategory2,
+            name="Clean Code",
+            slug="clean-code",
+            description="A book about how to write a clean code",
+            price=25,
         )
--- myshop/tests/shop/test_views.py
+++ myshop/tests/shop/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from .test_model_mixin_testcase import ModelMixinTestCase
+from shop.models import Product
 
 
 class TestViews(ModelMixinTestCase, TestCase):
@@ -22,7 +23,31 @@ class TestViews(ModelMixinTestCase, TestCase):
     def test_return_404_for_invalid_slug(self):
         self.product_detail_url = reverse(
             "shop:product_detail",
-            args=[self.newproduct.id, "invalid_slug"],
+            args=[self.newproduct.id, "invalid_product_slug"],
         )
         response = self.client.get(self.product_detail_url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_post_list_shows_all_product_when_category_not_selected(self):
+        self.all_products = Product.objects.all()
+        request = self.client.get(reverse("shop:product_list"))
+        response = request.context.get("products")
+        self.assertQuerysetEqual(self.all_products, response)
+
+    def test_post_list_shows_category_based_products_when_category_is_selected(
+        self,
+    ):
+        self.category_book = Product.objects.filter(category=self.newcategory2)
+        request = self.client.get(
+            reverse("shop:product_list_by_category", args=["books"])
+        )
+        response = request.context.get("products")
+        self.assertQuerysetEqual(self.category_book, response)
+
+    def test_post_list_shows_404_when_category_does_not_exists(self):
+        response = self.client.get(
+            reverse(
+                "shop:product_list_by_category", args=["invalid_category_slug"]
+            )
+        )
         self.assertEquals(response.status_code, 404)
