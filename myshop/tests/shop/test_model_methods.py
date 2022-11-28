1f92b2382b1dca0d3177445cde9883bb37f12423
Karthik <karthikeyan@testpress.in>
2022-11-28 17:02:03 +0530

    Refactor Testcases

--- myshop/tests/shop/test_model_methods.py
+++ myshop/tests/shop/test_model_methods.py
@@ -5,18 +5,18 @@ from django.urls import reverse
 
 class TestModelMethod(ModelMixinTestCase, TestCase):
     def test_absolute_url_in_category_model(self):
-        self.product_list_category = reverse(
+        product_list_category = reverse(
             "shop:product_list_by_category", args=[self.newcategory.slug]
         )
         self.assertEqual(
-            self.product_list_category, self.newcategory.get_absolute_url()
+            product_list_category, self.newcategory.get_absolute_url()
         )
 
     def test_absolute_url_in_product_model(self):
-        self.product_detail_view = reverse(
+        product_detail_view = reverse(
             "shop:product_detail",
             args=[self.newproduct.id, self.newproduct.slug],
         )
         self.assertEqual(
-            self.product_detail_view, self.newproduct.get_absolute_url()
+            product_detail_view, self.newproduct.get_absolute_url()
         )
