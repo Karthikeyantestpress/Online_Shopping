8fb967bc296dec04727e870a1c06286f3a79ca01
Karthik <karthikeyan@testpress.in>
2022-11-29 14:42:57 +0530

    Refactor : Add Testcases for cart view remove method

--- myshop/tests/cart/test_urls.py
+++ myshop/tests/cart/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from cart.views import cart_add, cart_detail
+from cart.views import cart_add, cart_detail, cart_remove
 
 
 class Testurls(SimpleTestCase):
@@ -11,3 +11,9 @@ class Testurls(SimpleTestCase):
     def test_cart_detail(self):
         cart_detail_url = reverse("cart:cart_detail")
         self.assertEqual((resolve(cart_detail_url).func), cart_detail)
+        self.cart_detail_url = reverse("cart:cart_detail")
+        self.assertEqual((resolve(self.cart_detail_url).func), cart_detail)
+
+    def test_cart_remove(self):
+        cart_remove_url = reverse("cart:cart_remove", args=[1])
+        self.assertEqual((resolve(cart_remove_url).func), cart_remove)
