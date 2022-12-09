b4ca1377f945026b69e67beca07b6499206e7656
Karthikeyan <112085200+Karthikeyantestpress@users.noreply.github.com>
2022-12-09 17:01:02 +0530

    Add support to checkout the cart (#8)
    * Add support to checkout the cart
    
    * Add Testcases for Order Create
    
    * Refactor Orderview

---
+++ myshop/tests/order/test_view.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from tests.shop.test_model_mixin_testcase import ModelMixinTestCase
+from django.urls import reverse
+
+
+class Testcreateview(ModelMixinTestCase, TestCase):
+    def test_order_create_template_when_http_method_is_post(self):
+        self.Order_Create_url = reverse("orders:order_create")
+
+        response = self.client.get(self.Order_Create_url)
+        self.assertTemplateUsed(response, "orders/order/create.html")
+
+    def test_order_create_template_when_http_method_is_get(self):
+        self.Order_Create_url = reverse("orders:order_create")
+
+        responsey = self.client.post(
+            self.Order_Create_url,
+            {
+                "first_name": "john",
+                "last_name": "smith",
+                "email": "deepak@gmail.com",
+                "address": "122,anhd, asjs,ask",
+                "postal_code": "605 110",
+                "city": "chennai",
+            },
+        )
+
+        self.assertTemplateUsed(responsey, "orders/order/created.html")
